1b13ae16ede6823ae600e9058285dfa01a14361e
Mauricio Chion <mauriciosaraiva2025@gmail.com>
2026-03-03 05:16:50 -0300

    Alteração da aula 03.03.26

--- app.py
+++ app.py
@@ -6,17 +6,29 @@ app = Flask(__name__)
 #Create, Read, Updade and Delete = Criar, Ler, Atualizar e Deletar
 #Tabela: Tarefa
 
-task = []
+tasks = []
 task_id_control = 1
 
 @app.route("/tasks", methods=["POST"])
 def create_task():
+    global task_id_control
     data = request.get_json()
     new_task = Task(id=task_id_control, title=data["title"],description=data.get("description", ""))
     task_id_control += 1
-    task.append(new_task)
+    tasks.append(new_task)
     print(tasks)
     return jsonify({"nessage": "Nova tarefa criada com sucesso"})
+
+@app.route("/tasks", methods=["GET"])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+
+    output = {
+                "tasks": task_list,
+                "total_tasks": 0
+    }
+    return jsonify(output)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
